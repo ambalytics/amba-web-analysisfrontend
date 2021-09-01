361b3d2785afc1fcac626bdf819c173634971fea
Klaus Kammerer <xlrx@users.noreply.github.com>
2021-09-01 14:38:18 +0200

    Update http-common.js

--- frontend/http-common.js
+++ frontend/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://194.163.156.203:8080/api",
+  baseURL: "https://api-analysis.ambalytics.com/api",
   headers: {
     "Content-type": "application/json"
   }
