d1bf24c5ffc1bcc7e92ace2a9bc0a11a2d4d5357
ci <ci@ambalytics.com>
2021-10-23 15:40:59 +0000

    deploy

---
+++ precache-manifest.d031ff932e7ff65139f8e00b7adea7ea.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b80bb307b1261867c9959a3b916df8",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "fb10da8b932b243846c4",
+    "url": "/css/Author~FieldsOfStudy~Publications.a5b4f210.css"
+  },
+  {
+    "revision": "bbd982d9823ba7189041",
+    "url": "/css/Publication.315ec643.css"
+  },
+  {
+    "revision": "31773e54652c82b746ac",
+    "url": "/css/about.acbc4467.css"
+  },
+  {
+    "revision": "b99cc77bc2a9b509f5d4",
+    "url": "/css/app.f9d40130.css"
+  },
+  {
+    "revision": "45d190c0264badff2587",
+    "url": "/css/chunk-vendors.435a61c1.css"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/fonts/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/fonts/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/fonts/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "513093985ede872b00c29b9296938359",
+    "url": "/img/android-icon-144x144.png"
+  },
+  {
+    "revision": "352475aacf47c61f6ec0a39cf5c6d161",
+    "url": "/img/android-icon-192x192.png"
+  },
+  {
+    "revision": "a1bb0e6937bea7785e6fc2884bd14733",
+    "url": "/img/android-icon-36x36.png"
+  },
+  {
+    "revision": "0d3c8bd008947a8edde3d90910dad0d5",
+    "url": "/img/android-icon-48x48.png"
+  },
+  {
+    "revision": "dfd5f9f462e044d2b6194191ee647a9c",
+    "url": "/img/android-icon-72x72.png"
+  },
+  {
+    "revision": "fe03a9800af75f0d444250a76e3bd4cd",
+    "url": "/img/android-icon-96x96.png"
+  },
+  {
+    "revision": "3e4b0707ae71b3bef3e51a87a169d509",
+    "url": "/img/apple-icon-114x114.png"
+  },
+  {
+    "revision": "e47efb031901d0869605a11d40d30127",
+    "url": "/img/apple-icon-120x120.png"
+  },
+  {
+    "revision": "513093985ede872b00c29b9296938359",
+    "url": "/img/apple-icon-144x144.png"
+  },
+  {
+    "revision": "83fcd8637811c739be066e20ffc2c0b5",
+    "url": "/img/apple-icon-152x152.png"
+  },
+  {
+    "revision": "83c1858625d89c8345ad40853bda1933",
+    "url": "/img/apple-icon-180x180.png"
+  },
+  {
+    "revision": "2c132d2e3aa1921dcf61c143ba04e140",
+    "url": "/img/apple-icon-57x57.png"
+  },
+  {
+    "revision": "e6133af7e539d0eb996ad8f9e022e3dc",
+    "url": "/img/apple-icon-60x60.png"
+  },
+  {
+    "revision": "dfd5f9f462e044d2b6194191ee647a9c",
+    "url": "/img/apple-icon-72x72.png"
+  },
+  {
+    "revision": "5ee3db669d09da7a5b65c68e206088fe",
+    "url": "/img/apple-icon-76x76.png"
+  },
+  {
+    "revision": "4f918c2f1050dc525011210514315f10",
+    "url": "/img/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "4f918c2f1050dc525011210514315f10",
+    "url": "/img/apple-icon.png"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/img/color.c7a33805.png"
+  },
+  {
+    "revision": "83d58cdf5dd9bed16e6efea9d1e615f0",
+    "url": "/img/crossref-logo.83d58cdf.svg"
+  },
+  {
+    "revision": "e606e2f94fe823bdebccd78e25599042",
+    "url": "/img/easy_explanation.e606e2f9.png"
+  },
+  {
+    "revision": "87272ef80feb702985618cf1af58f86c",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "c6850b61f51d688b9e811da0b8684f22",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "fe03a9800af75f0d444250a76e3bd4cd",
+    "url": "/img/favicon-96x96.png"
+  },
+  {
+    "revision": "aff42a4282666ff4fdce8e553918592b",
+    "url": "/img/homepage.png"
+  },
+  {
+    "revision": "f0ad77f1cbc1fd38d9bb1f64fe5595f1",
+    "url": "/img/logo.f0ad77f1.png"
+  },
+  {
+    "revision": "688c33807244b86c906fcfd98849ff88",
+    "url": "/img/logo_lj.688c3380.png"
+  },
+  {
+    "revision": "513093985ede872b00c29b9296938359",
+    "url": "/img/ms-icon-144x144.png"
+  },
+  {
+    "revision": "df2d377ac5df1afc37462a81a9875b9c",
+    "url": "/img/ms-icon-150x150.png"
+  },
+  {
+    "revision": "3acc2f6755c74e672ab655fdb56a91f8",
+    "url": "/img/ms-icon-310x310.png"
+  },
+  {
+    "revision": "33bd5e0e97715fcc96143be2e023ef10",
+    "url": "/img/ms-icon-70x70.png"
+  },
+  {
+    "revision": "f8d3e1ece4514d015c64ca41fc5ae452",
+    "url": "/img/open-aire-logo.f8d3e1ec.png"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/img/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "34255b8311ad23cb97078971309f111f",
+    "url": "/img/repo.34255b83.png"
+  },
+  {
+    "revision": "c11e3e7c13be894d5387fe6b5987c775",
+    "url": "/img/semantic_scholar-logo.c11e3e7c.svg"
+  },
+  {
+    "revision": "923455be1f667836547cad3dc57148eb",
+    "url": "/img/twitter-logo.923455be.svg"
+  },
+  {
+    "revision": "b74ad1efd4cbe99e3cc47f92fe298cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bb3bb8626db6937670c",
+    "url": "/js/Author.8c2433f5.js"
+  },
+  {
+    "revision": "48eb144bcc9c9b6e9355",
+    "url": "/js/Authors.2b224909.js"
+  },
+  {
+    "revision": "fb10da8b932b243846c4",
+    "url": "/js/Author~FieldsOfStudy~Publications.8b80dd83.js"
+  },
+  {
+    "revision": "78071c988ae173e91cc7",
+    "url": "/js/FieldsOfStudy.ec6bf936.js"
+  },
+  {
+    "revision": "bbd982d9823ba7189041",
+    "url": "/js/Publication.8405333e.js"
+  },
+  {
+    "revision": "f8d23429e81a7b996556",
+    "url": "/js/Publications.f48ea302.js"
+  },
+  {
+    "revision": "31773e54652c82b746ac",
+    "url": "/js/about.c71f9e75.js"
+  },
+  {
+    "revision": "b99cc77bc2a9b509f5d4",
+    "url": "/js/app.efe13e03.js"
+  },
+  {
+    "revision": "d4512f394ba762fddfac",
+    "url": "/js/chunk-2d21e38c.eed864a0.js"
+  },
+  {
+    "revision": "45d190c0264badff2587",
+    "url": "/js/chunk-vendors.f006a26e.js"
+  },
+  {
+    "revision": "3747e25ab2575c80c31ca6fbc0dca773",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
