081cc6d276b412ab3ef16a421eb937b50641736d
Lukas Jesche <ljaschka@gmail.com>
2021-10-26 08:23:26 +0200

    fix: title changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,14 @@
     <div class="layout-topbar p-d-flex p-shadow-3  p-jc-between  p-ai-center">
         <div class="logo-link">
             <router-link to="/" class="logo p-d-flex p-ai-center">
-                <img alt="logo" src="./assets/logo.png">
+                <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                    <g><rect fill="#0f6364" id="canvas_background" height="52" width="52" y="-1" x="-1"/></g>
+                     <g>
+                    <text xmlns="http://www.w3.org/2000/svg" style="font-family: 'Ubuntu', sans-serif;letter-spacing: -0.23em; font-weight: bolder; "
+                          font-weight="bold" xml:space="preserve" text-anchor="start" id="svg_1"
+                          fill="#ffffff" font-size="44" y="39" x="7">at</text>
+                     </g>
+                    </svg>
             </router-link>
         </div>
 
@@ -699,5 +706,59 @@
         margin: -30px 0 10px 0;
         height: 20px;
         color: $color-main;
+    }
+
+    /* cyrillic-ext */
+    @font-face {
+        font-family: 'Ubuntu';
+        font-style: normal;
+        font-weight: 500;
+        src: url(https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoCjC3jvWyNL4U.woff2) format('woff2');
+        unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+    }
+
+    /* cyrillic */
+    @font-face {
+        font-family: 'Ubuntu';
+        font-style: normal;
+        font-weight: 500;
+        src: url(https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoCjC3jtGyNL4U.woff2) format('woff2');
+        unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+    }
+
+    /* greek-ext */
+    @font-face {
+        font-family: 'Ubuntu';
+        font-style: normal;
+        font-weight: 500;
+        src: url(https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoCjC3jvGyNL4U.woff2) format('woff2');
+        unicode-range: U+1F00-1FFF;
+    }
+
+    /* greek */
+    @font-face {
+        font-family: 'Ubuntu';
+        font-style: normal;
+        font-weight: 500;
+        src: url(https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoCjC3js2yNL4U.woff2) format('woff2');
+        unicode-range: U+0370-03FF;
+    }
+
+    /* latin-ext */
+    @font-face {
+        font-family: 'Ubuntu';
+        font-style: normal;
+        font-weight: 500;
+        src: url(https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoCjC3jvmyNL4U.woff2) format('woff2');
+        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+    }
+
+    /* latin */
+    @font-face {
+        font-family: 'Ubuntu';
+        font-style: normal;
+        font-weight: 500;
+        src: url(https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoCjC3jsGyN.woff2) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
 </style>
